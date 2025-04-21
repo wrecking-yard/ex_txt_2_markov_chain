1bfee225b39ed6efef9261fd898f667f5afb3b17
Filip M. Nowak <github@temp.oneiroi.net>
2025-04-22 00:36:27 +0200

    type spec fix

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExTxt2MarkovChain.MixProject do
     [
       app: :ex_txt_2_markov_chain,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
